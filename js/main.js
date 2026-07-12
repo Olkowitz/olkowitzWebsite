fc5ef6d18f61040246dd243da313cb7e5082e4b4
Olkowitz Developer <dev@olkowitz.cz>
2026-07-12 13:06:44 +0200

    Initial commit for the structure

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Základní JavaScript pro olkowitz.cz
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Stránka olkowitz.cz načtena.');
+    
+    // Zde můžete přidat interakce, např. smooth scroll nebo menu
+});
